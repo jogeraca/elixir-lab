23d01b1a754c3d1910c14f880088b65213cefd96
Yoser <jogeraca@gmail.com>
2022-10-06 18:53:19 -0500

    refactor: queen attack

--- queens/lib/queen.ex
+++ queens/lib/queen.ex
@@ -1,49 +1,56 @@
 defmodule Queen do
-  defstruct [:color, :position]
-
-  def new(color: color, position: position) do
-    %{color: color, position: position}
-  end
-
+  @spec attack?(tuple(), tuple()) :: boolean
   def attack?(queen_black, queen_white) do
-    queen_movement?(queen_black.position, queen_white.position)
+    queen_movement?(queen_black, queen_white)
   end
 
-  def attack_with_obscacle?(queen_black, queen_white, [_ | _] = obstacles) do
-    queen_movement?(queen_black.position, queen_white.position) &&
-      !obstacle?(queen_black.position, obstacles)
+  @spec attack_with_obscacle?(tuple(), tuple(), nonempty_maybe_improper_list) :: boolean
+  def attack_with_obscacle?(queen_black_pos, queen_white_pos, [_ | _] = obstacles) do
+    queen_movement?(queen_black_pos, queen_white_pos) &&
+      !obstacle?(queen_black_pos, obstacles)
   end
 
-  def attack_with_check?(queen_black, queen_white, [_ | _] = obstacles, king_black, king_white) do
-    queen_movement?(queen_black.position, queen_white.position) &&
-      !obstacle?(queen_black.position, obstacles) &&
-      (check?(queen_black.position, king_white) ||
-         check?(queen_white.position, king_black))
+  @spec attack_with_check?(tuple(), tuple(), nonempty_maybe_improper_list, tuple(), tuple()) ::
+          boolean
+  def attack_with_check?(
+        queen_black_pos,
+        queen_white_pos,
+        [_ | _] = obstacles,
+        king_black,
+        king_white
+      ) do
+    queen_movement?(queen_black_pos, queen_white_pos) &&
+      !obstacle?(queen_black_pos, obstacles) &&
+      (check?(queen_black_pos, king_white) ||
+         check?(queen_white_pos, king_black))
   end
 
-  def obstacle?(position_queen, others) do
-    Enum.any?(others, &queen_movement?(&1, position_queen)) |> IO.inspect(label: :res)
+  @spec obstacle?(tuple(), list(tuple())) :: boolean
+  defp obstacle?(position_queen, others) do
+    Enum.any?(others, &queen_movement?(&1, position_queen))
   end
 
-  def check?(position_queen, position_king) do
+  @spec check?(tuple(), tuple()) :: boolean
+  defp check?(position_queen, position_king) do
     queen_movement?(position_queen, position_king)
   end
 
-  def queen_movement?({x1, y1}, {x2, y2}) when (x1 - x2) * -1 == y1 - y2 do
+  @spec queen_movement?(tuple(), tuple()) :: boolean
+  defp queen_movement?({x1, y1}, {x2, y2}) when (x1 - x2) * -1 == y1 - y2 do
     true
   end
 
-  def queen_movement?({x1, y1}, {x2, y2}) when x1 - x2 == y1 - y2 do
+  defp queen_movement?({x1, y1}, {x2, y2}) when x1 - x2 == y1 - y2 do
     true
   end
 
-  def queen_movement?({x, _}, {x, _}) do
+  defp queen_movement?({x, _}, {x, _}) do
     true
   end
 
-  def queen_movement?({_, y}, {_, y}) do
+  defp queen_movement?({_, y}, {_, y}) do
     true
   end
 
-  def queen_movement?(_, _), do: false
+  defp queen_movement?(_, _), do: false
 end
--- queens/test/queen_test.exs
+++ queens/test/queen_test.exs
@@ -4,54 +4,54 @@ defmodule QueenTest do
   alias Queen
 
   test "define_position/0" do
-    queen_black = Queen.new(color: :black, position: {1, 5})
-    queen_white = Queen.new(color: :white, position: {1, 3})
+    queen_black = {1, 5}
+    queen_white = {3, 3}
 
     assert Queen.attack?(queen_black, queen_white)
 
-    queen_black = Queen.new(color: :black, position: {2, 5})
-    queen_white = Queen.new(color: :white, position: {1, 5})
+    queen_black = {2, 5}
+    queen_white = {1, 5}
 
     assert Queen.attack?(queen_black, queen_white)
 
-    queen_black = Queen.new(color: :black, position: {0, 0})
-    queen_white = Queen.new(color: :white, position: {1, 7})
+    queen_black = {0, 0}
+    queen_white = {1, 7}
 
     refute Queen.attack?(queen_black, queen_white)
 
-    queen_black = Queen.new(color: :black, position: {3, 4})
-    queen_white = Queen.new(color: :white, position: {5, 6})
+    queen_black = {3, 4}
+    queen_white = {5, 6}
 
     assert Queen.attack?(queen_black, queen_white)
 
-    queen_black = Queen.new(color: :black, position: {0, 0})
-    queen_white = Queen.new(color: :white, position: {7, 7})
+    queen_black = {0, 0}
+    queen_white = {7, 7}
 
     assert Queen.attack?(queen_black, queen_white)
 
-    queen_black = Queen.new(color: :black, position: {0, 7})
-    queen_white = Queen.new(color: :white, position: {7, 0})
+    queen_black = {0, 7}
+    queen_white = {7, 0}
 
     assert Queen.attack?(queen_black, queen_white)
 
-    queen_black = Queen.new(color: :black, position: {0, 5})
-    queen_white = Queen.new(color: :white, position: {4, 1})
+    queen_black = {0, 5}
+    queen_white = {4, 1}
 
     assert Queen.attack?(queen_black, queen_white)
   end
 
   describe "attack_with_obstacle/3" do
     test "with obstacles" do
-      queen_black = Queen.new(color: :black, position: {1, 5})
-      queen_white = Queen.new(color: :white, position: {1, 3})
+      queen_black = {1, 5}
+      queen_white = {1, 3}
       other_pieces = [{2, 4}, {2, 3}, {2, 4}]
 
       refute Queen.attack_with_obscacle?(queen_black, queen_white, other_pieces)
     end
 
     test "without obstacles" do
-      queen_black = Queen.new(color: :black, position: {0, 7})
-      queen_white = Queen.new(color: :white, position: {7, 0})
+      queen_black = {0, 7}
+      queen_white = {7, 0}
       other_pieces = [{1, 4}, {2, 3}, {2, 4}]
 
       assert Queen.attack_with_obscacle?(queen_black, queen_white, other_pieces)
@@ -60,8 +60,8 @@ defmodule QueenTest do
 
   describe "attack_with_check/3" do
     test "with obstacles check" do
-      queen_black = Queen.new(color: :black, position: {1, 5})
-      queen_white = Queen.new(color: :white, position: {1, 3})
+      queen_black = {1, 5}
+      queen_white = {1, 3}
       other_pieces = [{2, 4}, {2, 3}, {2, 4}]
       king_black = {2, 5}
       king_white = {2, 2}
@@ -76,8 +76,8 @@ defmodule QueenTest do
     end
 
     test "without obstacles check" do
-      queen_black = Queen.new(color: :black, position: {0, 7})
-      queen_white = Queen.new(color: :white, position: {7, 0})
+      queen_black = {0, 7}
+      queen_white = {7, 0}
       other_pieces = [{1, 4}, {2, 3}, {2, 4}]
       king_black = {7, 2}
       king_white = {0, 6}
